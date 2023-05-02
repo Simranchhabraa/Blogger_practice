790a1ad0f2ccaefe451fe561db929a3eec6e884b
Simran Chhabra <simranchhabra098@gmail.com>
2023-05-02 18:58:44 +0530

    posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_165433) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_110557) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -25,6 +25,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_165433) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.integer "likes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taggings", force: :cascade do |t|
